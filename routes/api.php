f9ab0c65170325736d0699502210b58d70526ed5
Ghulam Rasool <imgrasool@gmail.com>
2023-05-13 18:33:15 +0500

    Updated routes/api.php, added CustomerController reference

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\V1\OrderController;
+use App\Http\Controllers\Api\V1\{
+    CustomerController,
+    OrderController
+};
 /*
 |--------------------------------------------------------------------------
 | API Routes
